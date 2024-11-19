69b1435e0cb6791dfd2ad4a3b53fe7caa72d1474
Dmitrii Davydov <davydovdru@gmail.com>
2024-11-19 19:24:01 +0300

    First

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello, World!")
+class Book:
+    name = ''
+    tags = [
+        """
+        /list[str]
+        """
+    ]
+    price = 0
+    def __init__(self):
+        pass
+
+class DataBase:
+    def searchByTags(*args) -> list[Book]: # Поиск похожих книг по тегам
+        pass
+    def getBook(*args) -> list[Book]: # Поиск таргетно одной книжки на разных сайтах
+        pass
+
+class Parser:
+    def searchBy(*args) -> list[Book]:
+        pass
+
+class InputOutput:
+    def getInput(*args):
+        pass
+    def output(*args):
+        pass
